d288f55ef9c689ba6b47da03019c17881e38bf0b
Edmilson de Azevedo Cassecasse <55765693+eacassecasse@users.noreply.github.com>
2025-04-07 18:15:54 +0000

    fix: Update baseURL for axios requests

--- frontend/spotterrods-web/src/lib/axios.ts
+++ frontend/spotterrods-web/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://spotter-rods.onrender.com/",
+  baseURL: "https://spotter-rods.onrender.com/api/v1/",
   headers: {
     "Content-Type": "application/json",
   },
