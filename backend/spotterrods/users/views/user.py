9a65fd7266729a41f1a55f20f44edd757f20bae3
Edmilson de Azevedo Cassecasse <55765693+eacassecasse@users.noreply.github.com>
2025-04-10 15:54:32 +0000

    fix: Change samesite policy to prevent frontend from blocking

--- backend/spotterrods/users/views/user.py
+++ backend/spotterrods/users/views/user.py
@@ -25,7 +25,7 @@ def _set_cookie(response, key, value):
             value=value,
             httponly=True,
             secure=True,
-            # samesite=ENV.get('DRF_AUTH_COOKIE_SAMESITE'),
+            samesite=ENV.get('DRF_AUTH_COOKIE_SAMESITE'),
             # domain=ENV.get('DRF_AUTH_COOKIE_DOMAIN'),
             # max_age=int(ENV.get('DRF_AUTH_COOKIE_MAXAGE'))
         )
