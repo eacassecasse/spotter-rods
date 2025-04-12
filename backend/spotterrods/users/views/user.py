c9660dcdf4b327e879d0969b95842371ffc51b9a
Edmilson de Azevedo Cassecasse <55765693+eacassecasse@users.noreply.github.com>
2025-04-12 21:13:50 +0000

    fix: Return the validation of the payload on login

--- backend/spotterrods/users/views/user.py
+++ backend/spotterrods/users/views/user.py
@@ -78,12 +78,25 @@ class UserLogin(TokenObtainPairView):
     permission_classes = [AllowAny]
 
     def post(self, request, *args, **kwargs):
+        serializer = UserLoginSerializer(data=request.data)
+        
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        
+        user = serializer.validated_data['user']
+
+        if not user:
+            return Response(
+                {"message": "Invalid username or password"},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+            
         response = super().post(request, *args, **kwargs)
         
         if response.status_code == 200:
             access_token = response.data['access']
             refresh_token = response.data['refresh']
-            user = RefreshToken(response.data['refresh'])['user']
             
             cache.set(f"refresh_token_{user.id}", refresh_token, timeout=7*24*3600)
             
